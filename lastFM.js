46d16c904d33aacf803fdf524ec88268facaea2b
Paul Schaefer <yeppaul2@gmail.com>
2013-12-16 18:18:03 -0500

    Update lastFM.js
    Made it a LOT smaller, added instructions

--- lastFM.js
+++ lastFM.js
@@ -1,21 +1,5 @@
-/* Loads most recently listened to tracks from Last.fm */
-/* You'll need jQuery */
-
-$(document).ready(function()) {
-	$.ajax({
-		type: "GET",
-		/* Get this from the last.fm API */
-		url: "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=paul_r_schaefer&api_key=b25b959554ed76058ac220b7b2e0a026&limit=3",
-		dataType: "xml",
-		success: showListens
-	});
+// Be sure to replace [USERNAME] and [API]
+$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=[USERNAME]&api_key=[API]&format=json', function(data) {
+	var song = $(".song"), artistVal = data.recenttracks.track[0].artist["#text"], trackVal = data.recenttracks.track[0].name;
+	song.append(artistVal + " \u2014 " + trackVal);
 });
-
-function showListens(xml) {
-	/* You have to put :not([nowplaying]) or Last.fm sends 2 tracks if you're listening to one at the moment */
-	$(xml).find("track:not([nowplaying])").each(function()
-	{
-		/* Create an empty div with the class listen */
-		$(".listen").append("<a href=" + $(this).find("url").text() + ">" + $(this).find("artist").text() + " &mdash; " + $(this).find("name").text() + "</a><br /><br />");	
-	});
-}

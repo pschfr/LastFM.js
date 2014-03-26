a847408eda882ec02dd0948f692320e03ad158e9
Paul Schaefer <yeppaul2@gmail.com>
2014-03-26 12:19:04 -0400

    Made song title a link to the Last.fm page

--- lastFM.js
+++ lastFM.js
@@ -1,8 +1,8 @@
 // Be sure to replace [USERNAME] and [API]
 function listens() {
 	$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=[USERNAME]&api_key=[API]&format=json', function(data) {
-		var song = $(".song"), artistVal = data.recenttracks.track[0].artist["#text"], trackVal = data.recenttracks.track[0].name;
-		song.html(artistVal + " \u2014 " + trackVal);
+		var song = $(".song"), artistVal = data.recenttracks.track[0].artist["#text"], trackVal = data.recenttracks.track[0].name, urlVal = data.recenttracks.track[0].url;
+		song.html("<a href='" + urlVal + "' target='_blank'>" + artistVal + " \u2014 " + trackVal + "</a>");
 	});
 }
 listens();

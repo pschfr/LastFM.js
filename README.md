30269200b69f9f5307dc90fd33b034bba2725cfe
Paul Schaefer <yeppaul2@gmail.com>
2016-08-23 01:27:47 -0400

    Vanilla JS update, much more coming soon

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-1. Get [jQuery](http://jquery.com/) (duh).
-2. Get an API key from [Last.fm](http://www.last.fm/api/account/create).
+1. Get an API key from [Last.fm](http://www.last.fm/api/account/create).
 3. Put the API key and your username in lastFM.js.
-4. Place a `<span class="song"></span>` wherever in your HTML.
+4. Place a `<span id="lastFM"></span>` wherever in your HTML.
 5. Done. Go enjoy some tunes.
 
 It even auto-refreshes! :D
--- lastFM.js
+++ lastFM.js
@@ -1,14 +1,24 @@
-// Replace user and key with your own. Or use mine, it's up to you.
-function listens() {
-	var user = 'paul_r_schaefer', key = '0f680404e39c821cac34008cc4d803db';
-	$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + key + '&format=json', function(data) {
-		$(".song").html(data.recenttracks.track[0].artist["#text"] + " \u2014 " + data.recenttracks.track[0].name);
-	});
+function lastFM_request() {
+	var method = 'user.getrecenttracks';
+	var user   = 'paul_r_schaefer';
+	var key    = '0f680404e39c821cac34008cc4d803db';
+	var lastFMurl = 'https://ws.audioscrobbler.com/2.0/?method=' + method + '&user=' + user + '&api_key=' + key + '&format=json';
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('GET', lastFMurl, true);
+	xmlhttp.onreadystatechange = function() {
+	    if (xmlhttp.readyState == 4) {
+	        if(xmlhttp.status == 200) {
+	            var obj = JSON.parse(xmlhttp.responseText);
+				console.log(obj.recenttracks.track[0]);
+
+				var artistName = obj.recenttracks.track[0].artist['\#text'];
+				var songName   = obj.recenttracks.track[0].name;
+				document.getElementById('lastFM').innerHTML = artistName + ' - ' + songName;
+	         }
+	    }
+	};
+	xmlhttp.send(null);
 }
-$(document).ready(function(){
-	// do the rest of your page functionality here
-}).bind("loaded", function(){
-	listens();
-	setInterval(listens, 2000);
-	// 2000 is how many milliseconds between refresh
-});
+lastFM_request();
+setInterval(lastFM_request(), 30000);

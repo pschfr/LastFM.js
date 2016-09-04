3fbd738a755af04db7a1f8fc3abfaccaecafd17d
Paul Schaefer <yeppaul2@gmail.com>
2016-09-04 00:51:05 -0400

    added no album image

--- lastFM.js
+++ lastFM.js
@@ -3,7 +3,7 @@ function lastFM_request() {
 	var username  = 'paul_r_schaefer';
 	var API_key   = '0f680404e39c821cac34008cc4d803db';
 	var number    = '10'; // Increase this to increase number of tracks
-	var lastFMurl = 'https://ws.audioscrobbler.com/2.0/?method=' + method + '&user=' + username + '&api_key=' + API_key + '&format=json';
+	var lastFMurl = 'https://ws.audioscrobbler.com/2.0/?method=' + method + '&user=' + username + '&api_key=' + API_key + '&limit=' + number + '&format=json';
 	var element   = document.getElementById('lastFM');
 	var xmlhttp   = new XMLHttpRequest();
 
@@ -16,7 +16,7 @@ function lastFM_request() {
 				console.log(obj);
 
 				var total = obj.recenttracks['\@attr'].total;
-				element.innerHTML += '<strong>' + total + '</strong> tracks total<br><br>';
+				element.innerHTML += '<p><a href="http://www.last.fm/user/' + username + '" target="_blank">' + username + '</a> has scrobbled <strong>' + total + '</strong> tracks total.</p>';
 
 				for (i = 0; i < number; i++) {	// Loop through responses
 					var track   = obj.recenttracks.track[i]; // references this specific track
@@ -27,18 +27,17 @@ function lastFM_request() {
 					var albumName  = track.album['\#text'];
 					var songName   = track.name;
 					var songURL    = track.url;
-					var imgURL     = track.image[0]['\#text']; // Image sizes go from 0 - 3
-
-					if (track['\@attr']) // if attribute that holds now playing exists
-						var nowPlaying = track['\@attr'].nowplaying;
+					var imgURL     = track.image[1]['\#text']; // Image sizes go from 0 - 3
 
 					if (imgURL != '') // if imgURL exists, print image
 						element.innerHTML += '<img src="' + imgURL + '" alt="' + albumName + '" title="' + albumName + '"/> ';
+					else              	// or print blank album cover
+						element.innerHTML += '<img src="images/no-cover.png" alt="' + albumName + '" title="' + albumName + '"/> ';
 
 					// prints link to song with artist and song name
 					element.innerHTML += '<a href="' + songURL + '" target="_blank">' + artistName + ' - ' + songName + '</a> ';
 
-					if (track['\@attr'] && nowPlaying != '') // if attribute exists, and is now playing, print now playing gif
+					if (track['\@attr'] && track['\@attr'].nowplaying != '') // if now playing, print now playing gif
 						element.innerHTML += '<img src="scrobbling.gif" alt="Now Playing" title="Now Playing" />';
 
 					element.innerHTML += '<br>';
